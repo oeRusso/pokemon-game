a16c44b900b4b8fce624e42c2118619d8b611aa0
Esteban Russo <oerusso28@gmail.com>
2025-08-22 20:36:43 -0300

    feat(pokemon-options): refactor component to use dynamic options and improve styling

--- src/modules/pokemon/components/PokemonOptions.vue
+++ src/modules/pokemon/components/PokemonOptions.vue
@@ -1,15 +1,46 @@
 <template name="component-name">
-    <section class="mt-5">
-        <ul>
-            <li class="bg-white shadow-md rounded-lg p-3 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100">Pokemon 1</li>
-            <li class="bg-white shadow-md rounded-lg p-3 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100">Pokemon 2</li>
-            <li class="bg-white shadow-md rounded-lg p-3 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100">Pokemon 3</li>
-            <li class="bg-white shadow-md rounded-lg p-3 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100">Pokemon 4</li>
-        </ul>
+    <section class="mt-5 flex flex-col">
+     <button v-for="{name,id} in options" :key="id" @click="$emit('selectedOption', id)" class="capitalize">
+        <!-- bg-white shadow-md rounded-lg p-3 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100 -->
+        {{ name }}
+     </button>
     </section>
    
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Pokemon } from '../interfaces';
+
+    interface props {
+        options:Pokemon[]
+    }
+
+defineProps<props>();
+
+defineEmits<{
+    selectedOption : [id:number];
+}>();
+
+</script>
+
 <style scoped>
+ button{
+    background-color: white;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    border-radius: 0.5rem;
+    padding: 0.75rem;
+    margin: 0.5rem;
+    cursor: pointer;
+    width: 10rem;
+    text-align: center;
+    transition: all 0.15s ease-in-out;
+ }
+
+ button:hover {
+    background-color: #f3f4f6;
+ }
+
+ 
 </style>
+
+<!-- TODO: FIJARSE PQ EL POKEMON APARECE Y DEBE ESTAR EN NEGRO -->
--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,18 +1,19 @@
 <template>
-    <section v-if="isLoading || ramdomPokemon.id === null" class="flex flex-col justify-center items-center w-screen h-screen">
+    <section v-if="isLoading || ramdomPokemon.id === null"
+        class="flex flex-col justify-center items-center w-screen h-screen">
         <h1 class="text-3xl">Espere por favor</h1>
         <h3 class="animate-pulse">Cargando Pokemons</h3>
 
     </section>
 
-    <section v-else  class="flex flex-col justify-center items-center w-screen h-screen">
+    <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
         <h1 class="m-5">Quien en este pokemon?</h1>
         <h3>{{ ramdomPokemon}}</h3>
 
         <!-- pokemon picture -->
         <PokemonPicture :pokemon-id="ramdomPokemon.id" :show-pokemon="gameStatus === GameStatus.Playing" />
         <!-- pokemon options -->
-        <PokemonOptions/> 
+        <PokemonOptions :options="options" @selected-option="onSelectedOption"/>
 
 
     </section>
@@ -26,6 +27,9 @@ import { usePokemonGame } from '../composables/usePokemonGame';
 import { GameStatus } from '../interfaces';
 
 
-const {ramdomPokemon, isLoading, gameStatus} = usePokemonGame()
+const {ramdomPokemon, isLoading, gameStatus, pokemonOptions:options} = usePokemonGame()
 
+const onSelectedOption = (value: number) => {
+    console.log({value});
+     }
 </script>
